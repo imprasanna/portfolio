e0d408a5f266711ea7f1b0b9ff5c7bebffa05249
Prasanna Acharya <prasannaacharya2073@gmail.com>
2024-05-09 18:35:18 +0545

    Home page text styling maintained and some other stylings fixed

--- portfolio/src/components/home/bodyContent.jsx
+++ portfolio/src/components/home/bodyContent.jsx
@@ -7,17 +7,19 @@ function BodyContent() {
     <div className="container">
       <img className="illustration" src={illustration}></img>
       <div className="text-block">
-        <div className="text-main">
-          Hi there! I'm <br /> <strong>Prasanna Acharya</strong>
-        </div>
-        <div className="text-description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-          cupiditate soluta iste aperiam ipsam voluptatum ad doloremque dolor
-          quia vel commodi, cum omnis assumenda magnam voluptates maxime
-          consequatur libero totam animi molestias quasi repellat magni sed.
-          Officia quasi, ad nam soluta reiciendis enim, necessitatibus ab
-          aspernatur repellendus perferendis commodi quam deserunt beatae eum
-          nesciunt!
+        <div className="text">
+          <div className="text-main">
+            Hi there! I'm <br /> <span>.</span>
+          </div>
+          <div className="text-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+            cupiditate soluta iste aperiam ipsam voluptatum ad doloremque dolor
+            quia vel commodi, cum omnis assumenda magnam voluptates maxime
+            consequatur libero totam animi molestias quasi repellat magni sed.
+            Officia quasi, ad nam soluta reiciendis enim, necessitatibus ab
+            aspernatur repellendus perferendis commodi quam deserunt beatae eum
+            nesciunt!
+          </div>
         </div>
       </div>
     </div>
