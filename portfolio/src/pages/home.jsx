becccb7f3b2862c4641c0a17285a9256861ec086
Prasanna Acharya <prasannaacharya2073@gmail.com>
2024-05-09 17:08:47 +0545

    bodyContent file added in components

--- portfolio/src/pages/home.jsx
+++ portfolio/src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/home.scss";
 import Nav from "../components/nav";
+import BodyContent from "../components/bodyContent";
 
 function Home() {
   return (
     <div className="home">
       <Nav />
+      <BodyContent />
     </div>
   );
 }
