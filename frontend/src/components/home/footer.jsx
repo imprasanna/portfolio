6014e06fb378ee807ea4c1aedfad56a0f713e3fa
Prasanna Acharya <prasannaacharya2073@gmail.com>
2024-07-09 18:03:40 +0545

    Home page made responsive

--- frontend/src/components/home/footer.jsx
+++ frontend/src/components/home/footer.jsx
@@ -8,22 +8,24 @@ import "../../styles/footer.scss";
 function Footer() {
   return (
     <div>
-      <div className="footer">
-        <a href="https://www.instagram.com/acharya.prasanna1024/">
-          <FiInstagram className="social-icon" />
-        </a>
+      <div className="footer-wrapper">
+        <div className="footer">
+          <a href="https://www.instagram.com/acharya.prasanna1024/">
+            <FiInstagram className="social-icon" />
+          </a>
 
-        <a href="https://www.linkedin.com/in/prasanna-acharya-30b84b227/">
-          <FaLinkedinIn className="social-icon" />
-        </a>
+          <a href="https://www.linkedin.com/in/prasanna-acharya-30b84b227/">
+            <FaLinkedinIn className="social-icon" />
+          </a>
 
-        <a href="https://github.com/imprasanna">
-          <FaGithub className="social-icon" />
-        </a>
+          <a href="https://github.com/imprasanna">
+            <FaGithub className="social-icon" />
+          </a>
 
-        <a href="https://twitter.com/prasanna1024">
-          <BsTwitterX className="social-icon" />
-        </a>
+          <a href="https://twitter.com/prasanna1024">
+            <BsTwitterX className="social-icon" />
+          </a>
+        </div>
       </div>
     </div>
   );
