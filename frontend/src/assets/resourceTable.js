3ab849690c7df1b0d7d28630c26d8b2742c2721e
Prasanna Acharya <prasannaacharya2073@gmail.com>
2024-07-09 19:43:49 +0545

    Resources page updated

---
+++ frontend/src/assets/resourceTable.js
@@ -0,0 +1,135 @@
+export const tableDocs = [
+  {
+    id: 1,
+    content: "Hacking tools",
+    link: "https://prasannas-organization-1.gitbook.io/hacking-tools",
+    description:
+      "It is a repository of curated list of hacking tools like nmap, metasploit, hydra, etc. with their proper description and commands.",
+  },
+  {
+    id: 2,
+    content: "OSINT",
+    link: "https://prasannas-organization-1.gitbook.io/osints",
+    description:
+      "OSINT (Open Source Intelligence) is a way of investigating through information and resources available publicly. It contains a well organized list of OSINT tools.",
+  },
+  {
+    id: 3,
+    content: "eJPTv2 notes",
+    link: "https://www.notion.so/oasis1x1/eJPTv2-ac2ca5313b4d40cdab867485d6e6cbea",
+    description:
+      "eJPT (e-learning Jr Penetration Tester) is a well-recognized certification for getting entry level job in offensive security. This repository contains the notes for preparing for and passing the eJPT exam.",
+  },
+  {
+    id: 4,
+    content: "100DaysOfLearning Bug Hunting",
+    link: "https://oasis1x1.notion.site/Bug-hunting-e628dcd682d14ae3b125de1f38c32f3d",
+    description:
+      "It contains different tools, resources, methodologies that I followed throughout the journey of learning bug hunting for 100 days.",
+  },
+];
+
+export const tableHuntingRes = [
+  {
+    id: 1,
+    content: "Nahamsec's github repository",
+    link: "https://github.com/nahamsec/Resources-for-Beginner-Bug-Bounty-Hunters",
+    category: "Overall resources",
+  },
+  {
+    id: 2,
+    content: "Bug bounty blueprint: a beginner's guide",
+    link: "https://www.bugbountyhunter.com/disclosed",
+    category: "Overall resources",
+  },
+  {
+    id: 3,
+    content: "Portswigger labs",
+    link: "https://hackerone.com/hacktivity/overview",
+    category: "Practice labs",
+  },
+  {
+    id: 4,
+    content: "Pentester land",
+    link: "https://pentester.land/writeups/",
+    category: "Writeups",
+  },
+  {
+    id: 5,
+    content: "Bug bounty hunting search engine",
+    link: "https://www.bugbountyhunting.com/",
+    category: "Writeups",
+  },
+  {
+    id: 6,
+    content: "Hackerone hactivity",
+    link: "https://hackerone.com/hacktivity/overview",
+    category: "Bug reports",
+  },
+  {
+    id: 7,
+    content: "Bug reader",
+    link: "https://bugreader.com/reports",
+    category: "Bug reports",
+  },
+  {
+    id: 8,
+    content: "Bugbountyhunter disclosed reports",
+    link: "https://www.bugbountyhunter.com/disclosed",
+    category: "Bug reports",
+  },
+  {
+    id: 9,
+    content: "Hacking and bug hunting books",
+    link: "https://drive.google.com/drive/folders/1R7CePdLAjJXFKLaCUYR691mP7vEwQ3Ra",
+    category: "Books",
+  },
+  {
+    id: 10,
+    content: "NahamSec yt",
+    link: "https://youtube.com/@NahamSec",
+    category: "Youtube",
+  },
+  {
+    id: 11,
+    content: "Jason Haddix yt",
+    link: "https://youtube.com/@jhaddix",
+    category: "Youtube",
+  },
+  {
+    id: 12,
+    content: "STÖK Fredrick yt",
+    link: "https://youtube.com/@STOKfredrik",
+    category: "Youtube",
+  },
+  {
+    id: 12,
+    content: "STÖK Fredrick yt",
+    link: "https://youtube.com/@STOKfredrik",
+    category: "Youtube",
+  },
+  {
+    id: 13,
+    content: "codingo yt",
+    link: "https://youtube.com/@codingo",
+    category: "Youtube",
+  },
+  {
+    id: 14,
+    content: "InsiderPhD",
+    link: "https://youtube.com/@InsiderPhD",
+    category: "Youtube",
+  },
+  {
+    id: 15,
+    content: "PwnFunction yt",
+    link: "https://youtube.com/@PwnFunction",
+    category: "Youtube",
+  },
+  {
+    id: 16,
+    content: "BugBountReportsExplained yt",
+    link: "https://www.youtube.com/@BugBountyReportsExplained",
+    category: "Youtube",
+  },
+];
--- frontend/src/pages/resources.jsx
+++ frontend/src/pages/resources.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "../components/home/nav";
 import "../styles/resources.scss";
+import { tableDocs, tableHuntingRes } from "../assets/resourceTable";
 
 const Resources = () => {
   return (
@@ -12,64 +13,18 @@ const Resources = () => {
           <thead>
             <tr>
               <th>Content</th>
-              <th>Link</th>
               <th>Description</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Hacking tools</td>
-              <td>
-                <a href="https://prasannas-organization-1.gitbook.io/hacking-tools/">
-                  https://prasannas-organization-1.gitbook.io/hacking-tools
-                </a>
-              </td>
-              <td>
-                It is a repository of curated list of hacking tools like nmap,
-                metasploit, hydra, etc. with their proper description and
-                commands.
-              </td>
-            </tr>
-            <tr>
-              <td>OSINT</td>
-              <td>
-                <a href="https://prasannas-organization-1.gitbook.io/osint/">
-                  https://prasannas-organization-1.gitbook.io/osint
-                </a>
-              </td>
-              <td>
-                OSINT (Open Source Intelligence) is a way of investigating
-                through information and resources available publicly. It
-                contains a well organized list of OSINT tools.
-              </td>
-            </tr>
-            <tr>
-              <td>eJPTv2 notes</td>
-              <td>
-                <a href="https://www.notion.so/oasis1x1/eJPTv2-ac2ca5313b4d40cdab867485d6e6cbea">
-                  https://www.notion.so/oasis1x1/eJPTv2-ac2ca5313b4d40cdab867485d6e6cbea
-                </a>
-              </td>
-              <td>
-                eJPT (e-learning Jr Penetration Tester) is a well-recognized
-                certification for getting entry level job in offensive security.
-                This repository contains the notes for preparing for and passing
-                the eJPT exam.
-              </td>
-            </tr>
-            <tr>
-              <td>100DaysOfLearning Bug Hunting</td>
-              <td>
-                <a href="https://oasis1x1.notion.site/Bug-hunting-e628dcd682d14ae3b125de1f38c32f3d">
-                  https://oasis1x1.notion.site/Bug-hunting-e628dcd682d14ae3b125de1f38c32f3d
-                </a>
-              </td>
-              <td>
-                It contains different tools, resources, methodologies that I
-                followed throughout the journey of learning bug hunting for 100
-                days.
-              </td>
-            </tr>
+            {tableDocs.map((doc) => (
+              <tr key={doc.id}>
+                <td>
+                  <a href={doc.link}>{doc.content}</a>
+                </td>
+                <td>{doc.description}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
 
@@ -78,162 +33,18 @@ const Resources = () => {
           <thead>
             <tr>
               <th>Content</th>
-              <th>Link</th>
               <th>Category</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Nahamsec's github repository</td>
-              <td>
-                <a href="https://github.com/nahamsec/Resources-for-Beginner-Bug-Bounty-Hunters">
-                  https://github.com/nahamsec/Resources-for-Beginner-Bug-Bounty-Hunters
-                </a>
-              </td>
-              <td>Overall resources</td>
-            </tr>
-
-            <tr>
-              <td>Bug bounty blueprint: a beginner's guide</td>
-              <td>
-                <a href="https://www.bugbountyhunter.com/disclosed">
-                  https://www.bugbountyhunter.com/disclosed
-                </a>
-              </td>
-              <td>Overall resources</td>
-            </tr>
-
-            <tr>
-              <td>Portswigger labs</td>
-              <td>
-                <a href="https://hackerone.com/hacktivity/overview">
-                  https://hackerone.com/hacktivity/overview
-                </a>
-              </td>
-              <td>Practice labs</td>
-            </tr>
-
-            <tr>
-              <td>Pentester land</td>
-              <td>
-                <a href="https://drive.google.com/drive/folders/1R7CePdLAjJXFKLaCUYR691mP7vEwQ3Ra">
-                  https://drive.google.com/drive/folders/1R7CePdLAjJXFKLaCUYR691mP7vEwQ3Ra
-                </a>
-              </td>
-              <td>Writeups</td>
-            </tr>
-
-            <tr>
-              <td>Bug bounty hunting search engine</td>
-              <td>
-                <a href="https://www.bugbountyhunting.com/">
-                  https://www.bugbountyhunting.com/
-                </a>
-              </td>
-              <td>Writeups</td>
-            </tr>
-
-            <tr>
-              <td>Hackerone hactivity</td>
-              <td>
-                <a href="https://hackerone.com/hacktivity/overview">
-                  https://hackerone.com/hacktivity/overview
-                </a>
-              </td>
-              <td>Bug reports</td>
-            </tr>
-
-            <tr>
-              <td>Bug reader</td>
-              <td>
-                <a href="https://drive.google.com/drive/folders/1R7CePdLAjJXFKLaCUYR691mP7vEwQ3Ra">
-                  https://drive.google.com/drive/folders/1R7CePdLAjJXFKLaCUYR691mP7vEwQ3Ra
-                </a>
-              </td>
-              <td>Bug reports</td>
-            </tr>
-
-            <tr>
-              <td>Bugbountyhunter disclosed reports</td>
-              <td>
-                <a href="https://www.bugbountyhunter.com/disclosed">
-                  https://www.bugbountyhunter.com/disclosed
-                </a>
-              </td>
-              <td>Bug reports</td>
-            </tr>
-
-            <tr>
-              <td>Hackerone hactivity</td>
-              <td>
-                <a href="https://hackerone.com/hacktivity/overview">
-                  https://hackerone.com/hacktivity/overview
-                </a>
-              </td>
-              <td>Bug reports</td>
-            </tr>
-
-            <tr>
-              <td>Hacking and bug hunting books</td>
-              <td>
-                <a href="https://drive.google.com/drive/folders/1R7CePdLAjJXFKLaCUYR691mP7vEwQ3Ra">
-                  https://drive.google.com/drive/folders/1R7CePdLAjJXFKLaCUYR691mP7vEwQ3Ra
-                </a>
-              </td>
-              <td>Books</td>
-            </tr>
-
-            <tr>
-              <td>Youtube channels</td>
-              <td>
-                <ul>
-                  <li>
-                    <a href="https://youtube.com/@NahamSec">
-                      https://youtube.com/@NahamSec
-                    </a>{" "}
-                    (NahamSec)
-                  </li>
-                  <li>
-                    <a href="https://youtube.com/@jhaddix">
-                      https://youtube.com/@jhaddix
-                    </a>{" "}
-                    (jhaddix)
-                  </li>
-                  <li>
-                    <a href="https://youtube.com/@STOKfredrik">
-                      https://youtube.com/@STOKfredrik
-                    </a>{" "}
-                    (STÖK)
-                  </li>
-                  <li>
-                    <a href="https://youtube.com/@codingo">
-                      https://youtube.com/@codingo
-                    </a>{" "}
-                    (codingo)
-                  </li>
-                  <li>
-                    <a href="https://youtube.com/@InsiderPhD">
-                      https://youtube.com/@InsiderPhD
-                    </a>{" "}
-                    (InsiderPhD)
-                  </li>
-                  <li>
-                    <a href="https://www.youtube.com/@PwnFunction">
-                      https://www.youtube.com/@PwnFunction
-                    </a>{" "}
-                    (PwnFunction)
-                  </li>
-                  <li>
-                    <a href="https://www.youtube.com/@BugBountyReportsExplained">
-                      https://www.youtube.com/@BugBountyReportsExplained
-                    </a>{" "}
-                    <br />
-                    (BugBountyReportsExplained)
-                  </li>
-                </ul>
-              </td>
-              <td>Youtube</td>
-            </tr>
+            {tableHuntingRes.map((resource) => (
+              <tr key={resource.id}>
+                <td>
+                  <a href={resource.link}>{resource.content}</a>
+                </td>
+                <td>{resource.category}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
