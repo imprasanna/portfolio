7d5b358ac87519d6883dc19d380e2ccf43185b9a
Prasanna Acharya <prasannaacharya2073@gmail.com>
2024-09-30 14:44:06 +0545

    site logo changed

--- src/components/home/nav.jsx
+++ src/components/home/nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/hack.jpg";
 import "../../styles/nav.scss";
 import { Link, useLocation } from "react-router-dom";
 import DropdownMenu from "./dropDownMenu";
